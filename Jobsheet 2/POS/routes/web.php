b7eabd74284aa2f86b431e63f4649fc723ea79c1
Rakagali Resda Krisandi Putra <faesalgali@gmail.com>
2026-03-02 19:03:21 +0700

    Praktikum-5

--- Jobsheet 2/POS/routes/web.php
+++ Jobsheet 2/POS/routes/web.php
@@ -1,22 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\ProductController;
-use App\Http\Controllers\UserController;
-use App\Http\Controllers\SalesController;
+use App\Http\Controllers\LevelController;
+use App\Http\Controllers\KategoriController;
 
-// Home
-Route::get('/', [HomeController::class, 'index']);
-
-// Products
-Route::get('/products', [ProductController::class, 'index']);
-Route::prefix('category')->group(function () {
-    Route::get('/{name}', [ProductController::class, 'category']);
+Route::get('/', function() {
+    return view('welcome');
 });
 
-// User
-Route::get('/user/{id}/name/{name}', [UserController::class, 'profile']);
+Route::get('/level', [LevelController::class,'index']);
+Route::get('/kategori', [KategoriController::class, 'index']);
 
-// Penjualan
-Route::get('/sales', [SalesController::class, 'index']);
