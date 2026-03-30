7c171098ae2cbd8d9e3b6ec44ea310d22a3514d4
Rakagali Resda Krisandi Putra <faesalgali@gmail.com>
2026-03-30 11:12:51 +0700

    praktikum 2.7

--- Jobsheet 2/POS/app/Models/UserModel.php
+++ Jobsheet 2/POS/app/Models/UserModel.php
@@ -7,9 +7,16 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class UserModel extends Model
 {
-    use HasFactory;
-
     protected $table = 'm_user';
     protected $primaryKey = 'user_id';
+    /**
+     * The attributes that are mass assignable.
+     * * @var array
+     */
     protected $fillable = ['level_id', 'username', 'nama', 'password'];
+
+    public function level(): BelongsTo // (Catatan: Pastikan Anda mengimpor use Illuminate\Database\Eloquent\Relations\BelongsTo; di atas class jika menggunakan type hinting BelongsTo ini)
+    {
+        return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
+    }
 }
