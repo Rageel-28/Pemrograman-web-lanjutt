b7978d9067a4ed09c14f701c4f33ab8b2cd2650c
Rakagali Resda Krisandi Putra <faesalgali@gmail.com>
2026-03-30 10:17:50 +0700

    praktikum 2.4

--- Jobsheet 2/POS/app/Http/Controllers/UserController.php
+++ Jobsheet 2/POS/app/Http/Controllers/UserController.php
@@ -9,12 +9,17 @@ class UserController extends Controller
 {
     public function index()
     {
-        // Hitung jumlah user dengan level_id 2
-        $user = UserModel::where('level_id', 2)->count();
+        $user = UserModel::firstOrNew(
+            [
+                'username' => 'manager33',
+                'nama' => 'Manager Tiga Tiga',
+                'password' => Hash::make('12345'),
+                'level_id' => 2
+            ]
+        );
         
-        // Hapus atau comment fungsi dd() agar view dapat dirender
-        // dd($user); 
-        
+        $user->save(); 
+
         return view('user', ['data' => $user]);
     }
     
